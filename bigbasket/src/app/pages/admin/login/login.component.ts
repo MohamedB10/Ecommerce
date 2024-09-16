24e2c64cb6c726a0087b02a648a6ec422b8b3089
MohamedB10 <bouharimohamed91@gmail.com>
2024-09-16 14:27:12 +0100

    Ajout des éléments de la page de login et d'inscription.

--- bigbasket/src/app/pages/admin/login/login.component.ts
+++ bigbasket/src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent  {
+  
+  loginObj: any = {
+    username: '',
+    password: ''
+  }
 
 }
